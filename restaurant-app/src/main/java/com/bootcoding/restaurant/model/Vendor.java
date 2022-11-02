efe89190d8343b97c4bb65a754f8e9f0cb9d2fed
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-02 16:18:14 +0530

    vendor Fields Added

--- restaurant-app/src/main/java/com/bootcoding/restaurant/model/Vendor.java
+++ restaurant-app/src/main/java/com/bootcoding/restaurant/model/Vendor.java
@@ -1,5 +1,22 @@
 package com.bootcoding.restaurant.model;
 
-public class Vendor {
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+public class Vendor
+{
+    private int id;
+    private String vendorOwner;
+    private String vendorName;
+    private long phone;
+    private String address;
+    private String email;
+    private String userId;
+    private String state;
+    private String city;
+    private Date created;
+    private Date modified;
 }
 
