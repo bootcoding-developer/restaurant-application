16daa38c33ad7cc045c4ca6e49f3f0daecc1c0fd
Anshul0707 <anshul.markwade786@gmail.com>
2022-11-04 16:15:18 +0530

    vendor Data Added..

--- restaurant-app/src/main/java/com/bootcoding/restaurant/controller/LoginController.java
+++ restaurant-app/src/main/java/com/bootcoding/restaurant/controller/LoginController.java
@@ -3,7 +3,6 @@ package com.bootcoding.restaurant.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
