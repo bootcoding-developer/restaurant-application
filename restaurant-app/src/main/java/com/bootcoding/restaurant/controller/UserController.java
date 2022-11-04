19096003328fe15ae8091d6d9703bf4aedd0bc71
bootcoding#123 <ModakAkshali211@gmail.com>
2022-11-04 12:48:53 +0530

    UserRegister

---
+++ restaurant-app/src/main/java/com/bootcoding/restaurant/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.bootcoding.restaurant.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Slf4j
+public class UserController {
+     @GetMapping("/user/register")
+    public void register(){
+         System.out.println("Enter your username:");
+     }
+
+}
